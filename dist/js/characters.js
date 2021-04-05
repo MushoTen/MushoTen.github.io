bc84e00a8c5df728e23c309f541ab959223a99f8
daishitie <daishitie@gmail.com>
2021-04-05 11:10:21 +0800

    update characters

--- dist/js/characters.js
+++ dist/js/characters.js
@@ -5,12 +5,12 @@ const elementSearch = urlParams.get("element");
 
 if (typeof raritySearch === `string`) {
     console.log(`Search rarity: ${raritySearch}`);
-    $("#searchResult").text(`Showing all ${raritySearch} star characters`);
+    $("#searchResult").text(`Showing all ${raritySearch}-star characters`);
 } else if (typeof elementSearch === `string`) {
     console.log(`Search element: ${elementSearch}`);
     $("#searchResult").text(`Showing all ${elementSearch} type characters`);
 } else {
-    $("#searchResult").text("Showing all characters");
+    $("#searchResult").text("Showing all 3-star characters");
 }
 
 jQuery.each(characters, function () {
@@ -23,12 +23,13 @@ jQuery.each(characters, function () {
             charactersAppend(this);
         }
     } else {
-        charactersAppend(this);
+        if (this.rarity === `3`) {
+            charactersAppend(this);
+        }
     }
 });
 
 function charactersAppend(character) {
-    console.log(character);
     let rarity = ``;
     for (var i = 1; i <= character.rarity; i++) {
         rarity += `<img class="inline-block w-4" src="./dist/img/stars/1.png" alt="Star">`;
@@ -36,38 +37,38 @@ function charactersAppend(character) {
 
     $("#characters").append(`
         <div class="bg-white shadow-sm rounded-xl">
-            <div class="pt-4 pl-4 flex items-center">
+            <div class="pt-2 px-2 flex items-center">
                 <div class="flex-shrink-0">
                     <img class="mx-auto h-36 rounded-2xl md:mx-0 sm:flex-shrink-0"
                         src="./dist/img/characters/${character.thumb}" alt="${character.name}">
                 </div>
                 <div class="pl-2">
                     <div class="uppercase tracking-wide text-sm text-amber-500 font-semibold">${character.title}</div>
-                    <a href="./character/${character.title}-${character.name}.html" class="text-2xl text-amber-900 hover:underline">${character.name}</a>
+                    <a class="text-2xl text-amber-900">${character.name}</a>
                     <p class="relative mt-1 text-sm text-gray-500">
-                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.tier.rank}</div>
-                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">
+                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.tier.rank}</div>
+                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">
                             <img class="inline-block w-4" src="./dist/img/elements/${character.element}.png" alt="${character.element}">
                             ${character.element}
                         </div>
-                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${rarity}</div>
+                        <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${rarity}</div>
                     </p>
                 </div>
             </div>
 
-            <div class="pt-2 pb-4 pl-4">
+            <div class="px-2 py-2">
                 <p class="text-sm text-gray-500">
                     Base Stats
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.base.hp} HP</div>
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.base.atk} ATK</div>
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.base.def} DEF</div>
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.base.spd} SPD</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.base.hp} HP</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.base.atk} ATK</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.base.def} DEF</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.base.spd} SPD</div>
                 </p>
-                <p class="mt-1 text-sm text-gray-500">
-                    Max Limit Break
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.mlb.hp} HP</div>
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.mlb.atk} ATK</div>
-                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-md">${character.mlb.def} DEF</div>
+                <p class="mt-2 text-sm text-gray-500">
+                    Max Limit Break Stats
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.mlb.hp} HP</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.mlb.atk} ATK</div>
+                    <div class="inline-block text-sm bg-gray-700 text-white py-0.5 px-2.5 rounded-lg">${character.mlb.def} DEF</div>
                 </p>
             </div>
         </div>
